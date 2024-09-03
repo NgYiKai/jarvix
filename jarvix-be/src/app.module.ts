86645794dbc3496d1194a159641af1b271ba784f
NgYiKai <62179279+NgYiKai@users.noreply.github.com>
2024-09-03 13:38:20 +0000

    Added task resource

--- jarvix-be/src/app.module.ts
+++ jarvix-be/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TaskModule } from './task/task.module';
+import { PrismaService } from './prisma.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), TaskModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,PrismaService],
 })
 export class AppModule {}
